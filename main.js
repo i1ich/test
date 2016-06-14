71e9a7bc844b41c5226d5fdee0ae1d20533c243b
Ilia Doinikov <ilidoin@gmail.com>
2016-06-14 11:04:28 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Names = [];
+var colors = ["red", "white"];
+var turn = 1;
+var CellNum;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+
+    socket.on('shoot', function (msg) {
+        io.emit('shoot', msg);
+    });
+    socket.on('n_user', function (msg) {
+        Names.push(msg);
+        io.emit('n_user', msg + "," + String(Names.length - 1));
+    });
+    socket.on('turn1', function (msg) {
+        CellNum = msg.split(",");
+        if (CellNum[1] == turn) {
+            console.log(msg + ',' + colors[turn]);
+            io.emit('n_user', msg + "," + colors[turn]);
+            turn = (turn + 1) % 2;
+        }
+
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
