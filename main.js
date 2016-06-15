2165472479c14c4cfb1c9addfe075b253eb5eb7d
Ilia Doinikov <ilidoin@gmail.com>
2016-06-15 10:31:21 +0300

    Update project

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var colors = ["red", "white"];
 var turn = 1;
 var CellNum;
 
+ const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -21,16 +22,16 @@ io.on('connection', function (socket) {
         io.emit('n_user', msg + "," + String(Names.length - 1));
     });
     socket.on('turn1', function (msg) {
-        CellNum = msg.split(",");
-        if (CellNum[1] == turn) {
-            console.log(msg + ',' + colors[turn]);
-            io.emit('n_user', msg + "," + colors[turn]);
-            turn = (turn + 1) % 2;
-        }
+        //CellNum = msg.split(",");
+        //if (CellNum[1] == turn) {
+        //    console.log(msg + ',' + colors[turn]);
+            io.emit('debug', (msg + "," + colors[turn]));
+        //    turn = (turn + 1) % 2;
+        //}
 
     });
 });
 
-http.listen(3000, function () {
+http.listen(port, function () {
     console.log('listening on *:3000');
 });
